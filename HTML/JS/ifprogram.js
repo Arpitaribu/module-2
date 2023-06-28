b33d882a87a5c1d9f8732f2607bc783adb4e0e7c
Arpitaribu <arpita.ribu@yahoo.com>
2023-06-28 12:47:16 +0530

    Initial commit

---
+++ HTML/JS/ifprogram.js
@@ -0,0 +1,25 @@
+a = 2
+if (a>80)
+{
+    console.log("A Grdae");
+}
+else if(a>60)
+{
+    console.log("B Grade");
+}
+else if(a>50)
+{
+    console.log("C Grade");
+}
+else if(a>40)
+{
+    console.log("D Grade");
+}
+else if(a>30)
+{
+    console.log("E Grade");
+}
+else if(a>20)
+{
+    console.log("FAIL");
+}
